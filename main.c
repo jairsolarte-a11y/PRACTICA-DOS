258261afe81dc6f936fa3ddf5ba4234f8844a5b7
jairsolarte-a11y <jairsolarte@unicauca.edu.co>
2026-04-20 22:53:44 -0500

    implemetación del módulo ADC

--- main.c
+++ main.c
@@ -1,31 +1,60 @@
 #include <xc.h>
 #include <stdint.h>
 #define _XTAL_FREQ 8000000
-// CONFIGURACIÓN
 
+// CONFIGURACIÓN
 #pragma config FOSC = HS
 #pragma config WDT = OFF
 #pragma config LVP = OFF
 #pragma config PBADEN = OFF
 
-// ================== PROTOTIPOS ==================
+// ================= PROTOTIPOS =================
 void IO_Init(void);
+void ADC_Init(void);
+unsigned int ADC_Read(unsigned char canal);
 
-// ================== MAIN ==================
+// ================= MAIN =================
 void main(void) {
 
     IO_Init();
+    ADC_Init();
+
+    unsigned int valor_adc;
 
     while(1) {
-        // Loop principal vacío por ahora
+
+        valor_adc = ADC_Read(0);  // Leer AN0
+
+        __delay_ms(1000);
     }
 }
 
-// ================== FUNCIONES ==================
+// ================= FUNCIONES =================
 void IO_Init(void) {
 
-    TRISA = 0xFF; // Entradas (sensores)
-    TRISD = 0x00; // Salidas (actuadores)
+    TRISA = 0xFF; // Entradas (AN0)
+    TRISD = 0x00; // Salidas
 
     LATD = 0x00;
+}
+
+// ================= ADC =================
+void ADC_Init() {
+
+    ADCON0 = 0x01;  // ADC ON, canal AN0
+    ADCON1 = 0x0E;  // AN0 analógico
+    ADCON2 = 0b10101010;
+}
+
+unsigned int ADC_Read(unsigned char canal) {
+
+    ADCON0 &= 0b11000101;
+    ADCON0 |= (canal << 2);
+
+    __delay_us(10);
+
+    GO_nDONE = 1;
+    while(GO_nDONE);
+
+    return (ADRESH << 8) | ADRESL;
 } 
